f7c08205629bf419cf94691965d3bcc3da719090
modembcc <modemhappy@gmail.com>
2024-03-06 23:47:38 +0800

    Set up website

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col items-center w-screen h-fit mt-8">
+        <p className="text-3xl">Welcome to TFT bootcamp</p>
+        <p className="text-xl">By modembcc</p>
+      </div>
+    </>
+  );
+}
